d5f0e7542b8f86eab8f289b56dbfe6308126956d
dixit pipalva <dixit@techvoot.com>
2023-04-17 20:03:07 +0530

    [200~HTML Table Demo, Js-<script>,

---
+++ tut1/index.js
@@ -0,0 +1,14 @@
+let x, y;
+x = 5 + 6;
+y = x * 11;
+document.getElementById("Demo").innerHTML = y;
+
+var a, b;
+x = 5 + 6;
+y = x * 10;
+document.getElementById("Demo1").innerHTML = y;
+
+let c;
+c = 5;
+document.getElementById("Demo2").innerHTML = c;
+
