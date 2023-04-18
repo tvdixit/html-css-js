e1cf8274af475919326c4e5beb55809ebf6a1ac2
dixit pipalva <dixit@techvoot.com>
2023-04-18 19:29:58 +0530

    Keywords; -Literalas; -variables; -operators; -identifiers;

--- tut1/index.js
+++ tut1/index.js
@@ -1,14 +1,51 @@
-let x, y;
-x = 5 + 6;
-y = x * 11;
-document.getElementById("Demo").innerHTML = y;
+// let x, y;
+// x = 5 + 6;
+// y = x * 11;
+// document.getElementById("Demo").innerHTML = y;
 
-var a, b;
-x = 5 + 6;
-y = x * 10;
-document.getElementById("Demo1").innerHTML = y;
+// var a, b;
+// x = 5 + 6;
+// y = x * 10;
+// document.getElementById("Demo1").innerHTML = y;
 
-let c;
-c = 5;
-document.getElementById("Demo2").innerHTML = c;
+// let c;
+// c = 5;
+// document.getElementById("Demo2").innerHTML = c;
+
+// variables:
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+// document.getElementById("Demo").innerHTML =  "the value of z is : " + z;
+
+// innerHTML:
+// document.getElementById("Demo").innerHTML = 5 + 6;
+
+//document.write():
+// document.write(5 + 6);
+
+// window.alert():
+// window.alert(5 + 6);
+
+// alert(5 + 6);
+
+// console.log:
+// it was in impact,
+// console.log(5 + 6);
+
+// operators use:
+// document.getElementById("Demo").innerHTML= (5 + 6) * 10;
+
+// ASSIGNMENT OPERATOR :
+// let x , y;
+// x = 5;
+// y = 6;
+// document.getElementById("Demo").innerHTML = x + y; 
+
+// Expression operator
+// document.getElementById("Demo").innerHTML = 5 * 10;
+
+
+
+
 
