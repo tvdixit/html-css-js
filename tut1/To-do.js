332ec3775e5cd75530bae6664a645e61ad7ca107
dixit pipalva <dixit@techvoot.com>
2023-04-28 19:26:40 +0530

    crud operator, add

--- tut1/To-do.js
+++ tut1/To-do.js
@@ -1,44 +1,51 @@
-window.onload = () => {
-    const form1 = document.querySelector("#Form");
+// const text = document.getElementById("fname");
+// const email = document.getElementById("email");
+// const number = document.getElementById("number");
+// const Reset = document.getElementById("Reset")
+// const edit = document.getElementById("edit")
 
-    let items = document.querySelector("items");
-    let submit = document.querySelector("Submit");
+// function submit(){
 
-    let editItem = null;
+//     const x = document.write(text.value);
+//     const y = document.write(email.value);
+//     const z = document.write(number.value);
+// }
+var mytodo_list = [];
+/*C*/
 
-    form1.addEventListener("submit", addItem);
-    items.addEventListener("click", removeItem);
-};
-
-if (e.target.classList.contains("Edit")) {
-    document.getElementById("item").value = e.target.parentNode.childNodes[0].data;
-    submit.value = "Edit";
-    editItem = e;
+function CreateTask() {
+    var task = document.getElementById("fname").value;
+    mytodo_list.push(task);
+    ReadAllTask();
 }
+/*R*/
 
-function addItem(e) {
-    e.preventDefault();
-
-    if (submit.value != "Submit") {
-        console.log("Hello");
-
-        editItem.target.parentNode.childNodes[0].data
-            = document.querySelector("item").value;
-
-        submit.value = "Submit";
-        document.querySelector("item").value = "";
-
-        document.querySelector("lblsuccess").innerHTML
-            = "Text edited successfully";
-
-        document.querySelector("lblsuccess")
-            .style.display = "block";
-
-        setTimeout(function () {
-            document.querySelector("lblsuccess")
-                .style.display = "none";
-        }, 3000);
-
-        return false;
+function ReadAllTask() {
+    var data = '';
+    for (var i = 0; i < mytodo_list.length; i++) {
+        data += '<tr>';
+        data += '<td>' + mytodo_list[i] + '</td>';
+        // data += '<td><button onclick=UpdateTask(' + i + ')>Update</button></td>';
+        // data += '<td><button onclick=DeleteTask(' + i + ')>Delete</button></td>';
+        data += '</tr>';
     }
+    document.getElementById("mytodo-tasks").innerHTML = data;
 }
+// /*U*/
+// function UpdateTask(item) {
+//     document.getElementById("UpdateForm").style.display = 'block';
+//     document.getElementById("fname").value = mytodo_list[item];
+//     document.getElementById("UpdateForm").onsubmit = function () {
+//         var task = document.getElementById("update-task").value;
+//         mytodo_list.splice(item, 1, task.trim());
+        
+//     }
+// }
+// /*D*/
+// function DeleteTask(item) {
+//     mytodo_list.splice(item, 1);
+//     ReadAllTask();
+// }
+// function CloseInput() {
+//     document.getElementById("UpdateForm").style.display = 'none';
+// }
