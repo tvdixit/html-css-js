689da0f5a9023884af342b849eb0f9f639241500
dixit pipalva <dixit@techvoot.com>
2023-04-29 19:56:00 +0530

    to-do list

--- tut1/To-do.js
+++ tut1/To-do.js
@@ -11,41 +11,90 @@
 //     const z = document.write(number.value);
 // }
 var mytodo_list = [];
+var updateInd = null
 /*C*/
 
 function CreateTask() {
     var task = document.getElementById("fname").value;
-    mytodo_list.push(task);
+    var email = document.getElementById("email").value;
+    var number = document.getElementById("number").value;
+    if (updateInd == null) {
+        mytodo_list.push({task: task, email: email, number: number});
+        updateInd = null
+        console.log('inside create task');
+    } else {
+        console.log('inside Update task');
+        let item = mytodo_list[updateInd]
+        item.task = task;
+        item.email = email;
+        item.number = number;
+
+    }
     ReadAllTask();
 }
+
 /*R*/
 
 function ReadAllTask() {
-    var data = '';
+    var data = '  ';
+    console.log(mytodo_list);
     for (var i = 0; i < mytodo_list.length; i++) {
         data += '<tr>';
-        data += '<td>' + mytodo_list[i] + '</td>';
-        // data += '<td><button onclick=UpdateTask(' + i + ')>Update</button></td>';
-        // data += '<td><button onclick=DeleteTask(' + i + ')>Delete</button></td>';
+        data += '<td>' + mytodo_list[i].task + '</td >' + '<td>' + mytodo_list[i].email + '</td >' + '<td>' + mytodo_list[i].number + '</td>';
+        data += '<td><button onclick = UpdateTask(' + i + ')> Update</button></td>';
+        // data += `<td><button onclick="UpdateTask(${i})">Update</button></td>`;
+        data += '<td><button onclick = DeleteTask(' + i + ')> Delete </button></td>';
         data += '</tr>';
     }
     document.getElementById("mytodo-tasks").innerHTML = data;
+    // ReadAllTask()
 }
-// /*U*/
-// function UpdateTask(item) {
+[{}]
+function UpdateTask(index) {
+    updateInd = index
+    let item = mytodo_list[index]
+    console.log(item.task);
+    console.log(item.email);
+    console.log(item.number);
+    console.log(index);
+    // document.getElementById("UpdateForm").style.display = 'block';
+    document.getElementById("fname").value = item.task;
+    document.getElementById("email").value = item.email;
+    document.getElementById("number").value = item.number;
+    // document.getElementById("UpdateForm").onsubmit = function UpdateTask() {
+    //     mytodo_list.splice(index, 1, item);
+    //     console.log('inside submit');
+    //     // ReadAllTask()
+    //     var task = document.getElementById("fname").value;
+    //     var email = document.getElementById("email").value;
+    //     var number = document.getElementById("number").value;
+    //     mytodo_list.splice(index, 1, task.trim(), email.trim(), number.trim());
+    //     ReadAllTask()
+    //     // document.getElementById("fname") = item.value;
+    //     // document.getElementById("email") = item.email;
+    //     // document.getElementById("number") = item.number;
+    //     // mytodo_list.splice(index, task.trim(), email.trim(), number.trim());
+    // }
+}
+// function UpdateTask(index, item) {
+//     // console.log(index);
+//     // console.log(item);
 //     document.getElementById("UpdateForm").style.display = 'block';
-//     document.getElementById("fname").value = mytodo_list[item];
+//     document.getElementById("fname").value = mytodo_list[item,index];
 //     document.getElementById("UpdateForm").onsubmit = function () {
-//         var task = document.getElementById("update-task").value;
-//         mytodo_list.splice(item, 1, task.trim());
-        
+
+//         var task = document.getElementById("fname").value;
+//         var email = document.getElementById("email").value;
+//         var number = document.getElementById("number").value;
+
+//         mytodo_list.splice(item, 1, task.trim(), email.trim(), number.trim());
 //     }
 // }
-// /*D*/
-// function DeleteTask(item) {
-//     mytodo_list.splice(item, 1);
-//     ReadAllTask();
-// }
-// function CloseInput() {
-//     document.getElementById("UpdateForm").style.display = 'none';
-// }
+
+function DeleteTask(item) {
+    mytodo_list.splice(item, 1);
+    ReadAllTask();
+}
+function CloseInput() {
+    document.getElementById("UpdateForm").style.display = 'none';
+}
