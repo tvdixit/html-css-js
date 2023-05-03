67648e171900a0ec42be09ea5f951d92b567906a
dixit pipalva <dixit@techvoot.com>
2023-05-03 19:18:04 +0530

    calculator upgrade

--- tut1/calculator.js
+++ tut1/calculator.js
@@ -8,7 +8,9 @@ function display(value) {
 
 function calculate() {
     var a = document.getElementById("result").value;
+    console.log("a", a);
     var b = eval(a);
+    console.log("b", b);
     document.getElementById("result").value = b;
 }
 
@@ -22,3 +24,22 @@ function ClearOne() {
     let res = document.getElementById("result");
     res.value = res.value.substr(0, res.value.length - 1)
 }
+
+// if (calc.result.value.includes("!")) {
+
+//     size = calc.result.value.length;
+//     n = Number(calc.result.value.substring(0, size - 1));
+//     f = 1;
+
+//     for (i = 2; i <= n; i++)
+//         f = f * i;
+//     calc.result.value = f;
+// }
+// function braces(){
+// let a = arr.forEach(value => {
+//     result.push(value in result
+//     ? options[value](...stack.splice(-2)): value);
+// });
+// }
+// return braces;
+// console.log(stackCalculator(arr));
